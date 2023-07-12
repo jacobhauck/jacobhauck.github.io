abaf7ca64538d19832034a030ba139abbfd85fce
Work <hauckjp@gmail.com>
2023-07-12 08:16:04 -0500

    Added some content

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Jacob's Website</title>
+            </Head>
+            <section>
+            My website
+            </section>
+        </>
+    );
+}

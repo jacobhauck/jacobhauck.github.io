1c9636d3c4781de27487ec6bd8e5291256ddd18e
Work <hauckjp@gmail.com>
2023-07-12 08:22:06 -0500

    Updated configuration for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+let assetPrefix = '';
+let basePath = '';
+
+if(process.env.GITHUB_ACTIONS) { 
+    const repo = 'jacobhauck';
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}/`
+}
+
+const nextConfig = {
+    assetPrefix: `/${repo}/`,
+    basePath: `/${repo}/`
+}
 
 module.exports = nextConfig

47f9274df5ceb847fb9333ae07bb397ccac5690c
Work <hauckjp@gmail.com>
2023-07-12 14:43:47 -0500

    Attempt to fix AGAIN

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-let assetPrefix = '';
-let basePath = '';
-
-if(process.env.GITHUB_ACTIONS || false) { 
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-    assetPrefix = `/${repo}`;
-    basePath = `/${repo}`;
-}
-
 const nextConfig = {
-    assetPrefix: assetPrefix,
-    basePath: basePath
+    output: "export",
+    basePath: "/jacobhauck.github.io",
+    images: {
+        unoptimized: true,
+    }
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;

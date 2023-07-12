fa6d8e2aef6171ccdfac36a145bf0dd634ac5780
Work <hauckjp@gmail.com>
2023-07-12 14:15:20 -0500

    Added footer and some main content.

---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+import style from '../styles/footer.module.css';
+
+export default function Footer() {
+    return (
+        <div className={style.footer}>
+            Last updated 12 July 2023.
+        </div>
+    );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import TopMatter from "@/components/topmatter";
 import Navbar from "@/components/navbar";
 import utilStyles from '../styles/utils.module.css'
+import Footer from "@/components/footer";
 
 export default function Home() {
     return (
@@ -12,6 +13,12 @@ export default function Home() {
             <section className={utilStyles.verticalFlex}>
                 <TopMatter />
                 <Navbar />
+                <section className={utilStyles.centeredText}>
+                    <br />
+                    <div>Welcome to my website!</div>
+                    <div>I'm not sure why you're here already, seeing as there is almost no content posted yet.</div>
+                </section>
+                <Footer />
             </section>
         </>
     );

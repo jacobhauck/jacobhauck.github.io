f8fcc3fc6aba3b1945829234044230909bd7c646
Work <hauckjp@gmail.com>
2023-07-12 15:07:06 -0500

    Attempt to fix yet again

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-
-const pathPrefix = process.env.NODE_ENV === 'production' ? '/jacobhauck' : '';
+const pathPrefix = process.env.NODE_ENV == 'production' ? '/jacobhauck' : '';
 
 const nextConfig = {
     output: "export",
     assetPrefix: pathPrefix,
     env: {
         pathPrefix
-    },
-    images: {
-        unoptimized: true,
     }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

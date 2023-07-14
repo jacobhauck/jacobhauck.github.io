171f409973d33205c661c53ad47a4e007f7f1e4b
Work <hauckjp@gmail.com>
2023-07-13 20:13:39 -0500

    Test again

--- out/_next/static/i4bEG70WeACMSZkwdC3MR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/caf5cc670e96cd4f.css","static/chunks/pages/index-886ff3336cf39d15.js"],"/_error":["static/chunks/pages/_error-a632b894a813ee6f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

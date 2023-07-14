fc8534cf3ffb1039e713f999b170eed228335e60
Work <hauckjp@gmail.com>
2023-07-13 20:32:19 -0500

    Add fancy date stuff

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,10 @@
 import style from '../styles/footer.module.css';
+import Date from '../components/date';
 
 export default function Footer() {
     return (
         <div className={style.footer}>
-            Last updated 12 July 2023.
+            Last updated <Date dateString={"2023-07-13"}
         </div>
     );
 }

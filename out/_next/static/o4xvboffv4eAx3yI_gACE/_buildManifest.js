04134280b28017db82010cf87e7748957b6a82fd
Work <hauckjp@gmail.com>
2023-07-13 20:25:47 -0500

    Set distDir to docs

--- out/_next/static/o4xvboffv4eAx3yI_gACE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/caf5cc670e96cd4f.css","static/chunks/pages/index-23454b4118c7cdad.js"],"/_error":["static/chunks/pages/_error-a632b894a813ee6f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
